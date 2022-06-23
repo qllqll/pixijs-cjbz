c8bd56d429354efef339b3fae8065e40c693a872
wwp <664282857@qq.com>
2022-06-23 17:19:10 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,239 @@
+var loader = new ImagesLoader()
+loader.loadImages(
+  [
+    'btn-l.png',
+    'btn-r.png',
+    'carpet.png',
+    'cloud1.png',
+    'cloud2.png',
+    'fn-b-flower.png',
+    'fn-cloth.png',
+    'fn-sepr-flower.png',
+    'fn-up-mask.png',
+    'girl.png',
+    'knock-human.png',
+    'main-bg.jpg',
+    'main-btn.png',
+    'main-title.png',
+    'run.png',
+    'sakura1.png',
+    'sakura2.png',
+    'sakura3.png',
+    'trans-bg.jpg',
+    'img/b-clothes1.png',
+    'img/b-clothes2.png',
+    'img/b-clothes3.png',
+    'img/b-clothes4.png',
+    'img/b-clothes5.png',
+    'img/b-clothes6.png',
+    'img/b-hair1.png',
+    'img/b-hair2.png',
+    'img/b-hair3.png',
+    'img/b-hair4.png',
+    'img/b-hair5.png',
+    'img/b-hair6.png',
+    'img/bg-clothes1.png',
+    'img/bg-clothes2.png',
+    'img/bg-clothes3.png',
+    'img/bg-clothes4.png',
+    'img/bg-clothes5.png',
+    'img/bg-clothes6.png',
+    'img/bg-hair1.png',
+    'img/bg-hair2.png',
+    'img/bg-hair3.png',
+    'img/bg-hair4.png',
+    'img/bg-hair5.png',
+    'img/bg-hair6.png',
+    'img/bg1.jpg',
+    'img/bg2.jpg',
+    'img/bg3.jpg',
+    'img/bg4.jpg',
+    'img/bg5.jpg',
+    'img/bg6.jpg',
+    'img/bg7.jpg',
+    'img/bg8.jpg',
+    'img/bg9.jpg',
+    'img/bm1.png',
+    'img/bm2.png',
+    'img/bm3.png',
+    'img/bmm1.png',
+    'img/bmm2.png',
+    'img/bmm3.png',
+    'img/bride-face.png',
+    'img/bridegroom-face.png',
+    'img/btn-del.png',
+    'img/btn-generate.png',
+    'img/btn-resize.png',
+    'img/cf1.png',
+    'img/cf2.png',
+    'img/cf3.png',
+    'img/cm1.png',
+    'img/cm2.png',
+    'img/cm3.png',
+    'img/dec1.png',
+    'img/dec2.png',
+    'img/dec3.png',
+    'img/dec4.png',
+    'img/dec5.png',
+    'img/dec6.png',
+    'img/dec7.png',
+    'img/dec8.png',
+    'img/dec9.png',
+    'img/dec10.png',
+    'img/dec11.png',
+    'img/dec12.png',
+    'img/fn-desc.png',
+    'img/nav-arrow.png',
+    'img/nav-dec.png',
+    'img/nav-human.png',
+    'img/nav-pet.png',
+    'img/nav-place.png',
+    'img/pet1.png',
+    'img/pet2.png',
+    'img/pet3.png',
+    'img/pet4.png',
+    'img/pet5.png',
+    'img/pet6.png',
+    'img/thumb-b-clothes1.png',
+    'img/thumb-b-clothes2.png',
+    'img/thumb-b-clothes3.png',
+    'img/thumb-b-clothes4.png',
+    'img/thumb-b-clothes5.png',
+    'img/thumb-b-clothes6.png',
+    'img/thumb-b-hair1.png',
+    'img/thumb-b-hair2.png',
+    'img/thumb-b-hair3.png',
+    'img/thumb-b-hair4.png',
+    'img/thumb-b-hair5.png',
+    'img/thumb-b-hair6.png',
+    'img/thumb-bg-clothes1.png',
+    'img/thumb-bg-clothes2.png',
+    'img/thumb-bg-clothes3.png',
+    'img/thumb-bg-clothes4.png',
+    'img/thumb-bg-clothes5.png',
+    'img/thumb-bg-clothes6.png',
+    'img/thumb-b-hair1.png',
+    'img/thumb-b-hair2.png',
+    'img/thumb-b-hair3.png',
+    'img/thumb-b-hair4.png',
+    'img/thumb-b-hair5.png',
+    'img/thumb-b-hair6.png',
+    'img/thumb-bg-clothes1.png',
+    'img/thumb-bg-clothes2.png',
+    'img/thumb-bg-clothes3.png',
+    'img/thumb-bg-clothes4.png',
+    'img/thumb-bg-clothes5.png',
+    'img/thumb-bg-clothes6.png',
+    'img/thumb-bg-hair1.png',
+    'img/thumb-bg-hair2.png',
+    'img/thumb-bg-hair3.png',
+    'img/thumb-bg-hair4.png',
+    'img/thumb-bg-hair5.png',
+    'img/thumb-bg-hair6.png',
+    'img/thumb-bm1.png',
+    'img/thumb-bm2.png',
+    'img/thumb-bm3.png',
+    'img/thumb-bmm1.png',
+    'img/thumb-bmm2.png',
+    'img/thumb-bmm3.png',
+    'img/thumb-chf1.png',
+    'img/thumb-chf2.png',
+    'img/thumb-chf3.png',
+    'img/thumb-chm1.png',
+    'img/thumb-chm2.png',
+    'img/thumb-chm3.png',
+    'img/thumb-d1.png',
+    'img/thumb-d2.png',
+    'img/thumb-d3.png',
+    'img/thumb-d4.png',
+    'img/thumb-d5.png',
+    'img/thumb-d6.png',
+    'img/thumb-d7.png',
+    'img/thumb-d8.png',
+    'img/thumb-d9.png',
+    'img/thumb-d10.png',
+    'img/thumb-d11.png',
+    'img/thumb-d12.png',
+    'img/thumb-hm1.png',
+    'img/thumb-hm2.png',
+    'img/thumb-hm3.png',
+    'img/thumb-hm4.png',
+    'img/thumb-hm5.png',
+    'img/thumb-hm6.png',
+    'img/thumb-p1.png',
+    'img/thumb-p2.png',
+    'img/thumb-p3.png',
+    'img/thumb-p4.png',
+    'img/thumb-p5.png',
+    'img/thumb-p6.png',
+    'img/thumb-s1.jpg',
+    'img/thumb-s2.jpg',
+    'img/thumb-s3.jpg',
+    'img/thumb-s4.jpg',
+    'img/thumb-s5.jpg',
+    'img/thumb-s6.jpg',
+    'img/thumb-s7.jpg',
+    'img/thumb-s8.jpg',
+    'img/thumb-s9.jpg',
+    'txt.png',
+    'fn-up-mask.png',
+    'pause.png',
+    'play.png',
+    'pixels.png',
+  ],
+  './images/'
+)
+loader.complete(function () {
+  console.log('completed')
+  $('.loading').hide()
+  $('.kv').show()
+})
+loader.process(function () {
+  $('.loadingNum').text(this.processNum + '%')
+  $('.line').css({ width: (18.875 * this.processNum) / 100 + 'rem' })
+  if (this.processNum > 60) {
+    $('.man').css({ visibility: 'hidden' })
+    $('.man2').show()
+    $('.woman').show()
+  } else {
+    $('.man').css({
+      transform: 'translate(' + (18.875 * this.processNum) / 100 + 'rem,0)',
+    })
+  }
+})
+loader.start()
+
+// 用户点击开始制作按钮，出现空白页，在出现场景布置页面
+function startMake() {
+  $('.kv').hide()
+  $('.gb').show()
+
+  //   文案分别进入
+  $('.txt1').animate(
+    {
+      right: '0',
+    },
+    700
+  )
+  $('.txt2').animate(
+    {
+      left: '0',
+    },
+    1000
+  )
+
+  setTimeout(() => {
+    //   告白出现
+    $('.gb').hide()
+    $('.bzcj').fadeIn('slow')
+  }, 1500)
+}
+
+$('.startBtn').bind('touchend', startMake)
+
+// 布置场景隐藏，显示海报页
+function showHaibao() {
+  $('.bzcj').hide()
+  $('.wy').show()
+}
+$('.saveBtn').bind('touchend', showHaibao)
