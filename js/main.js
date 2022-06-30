a4fd06dd65386461a628d75bbe18ac569e52a4c4
wwp <664282857@qq.com>
2022-06-30 10:45:33 +0800

    修改：     index.html 	新文件：   js/data.js 	新文件：   js/howler.min.js 	修改：     js/main.js 	新文件：   mp3/bg.mp3 	修改：     style/main.css 	修改：     style/main.less

--- js/main.js
+++ js/main.js
@@ -237,3 +237,485 @@ function showHaibao() {
   $('.wy').show()
 }
 $('.saveBtn').bind('touchend', showHaibao)
+
+// 页卡
+let tab = $('.tab')
+let tCont = $('.tCont')
+
+tab.each(function (i) {
+  $(this).click(function () {
+    tab.removeClass('selected')
+    tCont.removeClass('selected')
+    tab.eq(i).addClass('selected')
+    tCont.eq(i).addClass('selected')
+  })
+})
+
+// 场景布置代码
+
+// 创建舞台和默认背景
+
+const app = new PIXI.Application({
+  width: 750,
+  height: 1334,
+  transparent: true,
+})
+
+document.getElementById('stage').appendChild(app.view)
+
+let bgSpr = new PIXI.Sprite.fromImage('../images/img/bg1.jpg')
+bgSpr.position.set(0, 0)
+bgSpr.width = 750
+bgSpr.height = 1334
+bgSpr.name = 'bg'
+app.stage.addChild(bgSpr)
+
+// 点击页卡内容
+$('.tabCont img').each(function (i) {
+  $(this).click(function () {
+    let suid = $('.tabCont img').eq(i).attr('id')
+    // 风景0-8
+    if (i <= 8 && i >= 0) {
+      app.stage.getChildByName('bg').texture = new PIXI.Texture.fromImage(
+        `../images/img/bg${i + 1}.jpg`
+      )
+    }
+
+    // 人物9-14 9新郎 12新娘
+    if (i <= 14 && i >= 9) {
+      showSub(i, suid)
+    }
+
+    // 修饰15-26
+    if (i <= 26 && i >= 15) {
+      addSpr(2, suid)
+    }
+    // 宠物27-32
+    if (i <= 32 && i >= 27) {
+      addSpr(2, suid)
+    }
+  })
+})
+
+$('.subTab img').each(function (i) {
+  $(this).click(function () {
+    let suid = $('.subTab img').eq(i).attr('id')
+    // 新郎头发 0-5
+    if (i >= 0 && i <= 5) {
+      app.stage.getChildByName('xinlangSuper').children[0].children[2].texture =
+        new PIXI.Texture.fromImage(`../images/img/${suid}.png`)
+    }
+    // 新郎衣服 6-11
+    if (i >= 6 && i <= 11) {
+      app.stage.getChildByName('xinlangSuper').children[0].children[0].texture =
+        new PIXI.Texture.fromImage(`../images/img/${suid}.png`)
+    }
+    // 新娘头发 18-23
+    if (i >= 18 && i <= 23) {
+      app.stage.getChildByName(
+        'xinniangSuper'
+      ).children[0].children[2].texture = new PIXI.Texture.fromImage(
+        `../images/img/${suid}.png`
+      )
+    }
+    // 新娘衣服 24-29
+    if (i >= 24 && i <= 29) {
+      app.stage.getChildByName(
+        'xinniangSuper'
+      ).children[0].children[0].texture = new PIXI.Texture.fromImage(
+        `../images/img/${suid}.png`
+      )
+    }
+    // 伴郎 12-14  男童 15-17 伴娘 30-32 女童 33-35
+    if ((i >= 12 && i <= 17) || (i >= 30 && i <= 35)) {
+      addSpr(2, suid)
+    }
+  })
+})
+
+function showSub(type, picName) {
+  //9新郎 10伴郎  11男童  12新娘 13伴娘 14女童
+  $('div.subTab').hide()
+  switch (type) {
+    case 9:
+      $('div.subTab').eq(0).show()
+      addSpr(0)
+      break
+    case 12:
+      $('div.subTab').eq(3).show()
+      addSpr(1)
+      break
+    default:
+      let m = type - 9
+      $('div.subTab').eq(m).show()
+  }
+}
+
+// 添加不同的内容 特别注意：背景图片+人物部分
+function randomPos() {
+  return {
+    x: 750 / 2 + Math.random() * 40,
+    y: 1334 / 2 + Math.random() * 40,
+  }
+}
+let squ = 0
+
+// 清除选择状态
+function clearSelect() {
+  app.stage.children.forEach((item, i) => {
+    if (i == 0) return
+    item.children[1].visible = false
+    item.children[2].visible = false
+    item.children[3].visible = false
+  })
+}
+
+// type 0新郎 1新娘 2其他
+function addSpr(type, picName = '') {
+  clearSelect()
+
+  $('.subTab').hide()
+
+  let container = new PIXI.Container()
+
+  let cPosition = randomPos()
+  container.position.set(cPosition.x, cPosition.y)
+
+  // 边框。缩放按钮，删除按钮
+  const space = 10
+  const btnSize = 50
+
+  let borderline = new PIXI.Graphics()
+  borderline.name = 'borderline'
+  borderline.lineStyle(1, 0xaaaaaa, 1)
+  borderline.position.set(0, 0)
+
+  let resizeBtn = new PIXI.Sprite.fromImage('../images/img/btn-resize.png')
+  resizeBtn.name = 'resizeBtn'
+  resizeBtn.width = btnSize
+  resizeBtn.height = btnSize
+  resizeBtn.interactive = true
+
+  let delBtn = new PIXI.Sprite.fromImage('../images/img/btn-del.png')
+  delBtn.name = 'resizeBtn'
+  delBtn.interactive = true
+  delBtn.width = btnSize
+  delBtn.height = btnSize
+
+  let imgContainer = new PIXI.Container()
+  imgContainer.position.set(space / 2, space / 2)
+  imgContainer.interactive = true
+  // 总精灵组的宽高，borderline的宽高,resizeBtn的position,delBtn的position,imgContainer的name
+  let spaceBox = {}
+
+  //新郎新娘的唯一性
+  if (type == 0) {
+    let isExist = app.stage.children.find((item) => item.name == 'xinlangSuper')
+    if (isExist != undefined) {
+      alert('新郎角色只能添加一个！')
+      return
+    }
+    $('.subTab').eq(0).show()
+
+    container.name = 'xinlangSuper'
+    // 新郎
+    const male = { width: 340, height: 681 }
+    spaceBox = { width: male.width + space, height: male.height + space }
+
+    imgContainer.name = 'xinlang'
+
+    let mBody = new PIXI.Sprite.fromImage('../images/img/bg-clothes1.png')
+    mBody.position.set(0, 148)
+
+    let mFace = new PIXI.Sprite.fromImage('../images/img/bridegroom-face.png')
+    mFace.position.set(135, 2)
+
+    let mHair = new PIXI.Sprite.fromImage('../images/img/bg-hair1.png')
+    mHair.position.set(137, 0)
+    imgContainer.addChild(mBody)
+    imgContainer.addChild(mFace)
+    imgContainer.addChild(mHair)
+  }
+
+  if (type == 1) {
+    let isExist = app.stage.children.find(
+      (item) => item.name == 'xinniangSuper'
+    )
+    if (isExist != undefined) {
+      alert('新娘角色只能添加一个！')
+      return
+    }
+
+    $('.subTab').eq(3).show()
+
+    container.name = 'xinniangSuper'
+    // 新娘
+    const fmale = { width: 460, height: 886 }
+    spaceBox = { width: fmale.width + space, height: fmale.height + space }
+
+    imgContainer.name = 'xinniang'
+
+    let mBody = new PIXI.Sprite.fromImage('../images/img/b-clothes1.png')
+    mBody.position.set(0, 200)
+
+    let mFace = new PIXI.Sprite.fromImage('../images/img/bride-face.png')
+    mFace.position.set(140, 60)
+
+    let mHair = new PIXI.Sprite.fromImage('../images/img/b-hair1.png')
+    mHair.position.set(60, 0)
+    imgContainer.addChild(mBody)
+    imgContainer.addChild(mFace)
+    imgContainer.addChild(mHair)
+  }
+
+  if (type == 2) {
+    container.name = `sqrite${squ}`
+    squ++
+    // 其他
+    let imgSize = sizeJson[picName]
+    let imgSpr = new PIXI.Sprite.fromImage(`../images/img/${picName}.png`)
+    spaceBox = { width: imgSize.width, height: imgSize.height }
+    imgContainer.addChild(imgSpr)
+  }
+
+  borderline.width = container.width = spaceBox.width
+  borderline.height = container.height = spaceBox.height
+  borderline.drawRoundedRect(0, 0, spaceBox.width, spaceBox.height, 10)
+  container.pivot.set(spaceBox.width / 2, spaceBox.height / 2)
+  resizeBtn.position.set(spaceBox.width - btnSize / 2, -btnSize / 2)
+  delBtn.position.set(-btnSize / 2, spaceBox.height - btnSize / 2)
+
+  container.addChild(imgContainer)
+  container.addChild(borderline)
+  container.addChild(resizeBtn)
+  container.addChild(delBtn)
+
+  imgContainer
+    .on('touchstart', drapdropTouchStart)
+    .on('touchmove', drapdropTouchMove)
+    .on('touchend', clearTouchend)
+
+  resizeBtn.on('touchstart', resizeTouchStart)
+
+  delBtn.on('touchend', del)
+
+  app.stage.addChild(container)
+}
+
+// 拖放
+let startPos = {
+  x: 0,
+  y: 0,
+}
+
+let objPos = {
+  x: 0,
+  y: 0,
+}
+
+let flag = true // 是否可以执行拖拽的操作
+let sprName = '' // 执行拖拽的精灵组的别名
+
+function drapdropTouchStart(e) {
+  // 清除选择状态
+  clearSelect()
+  $('.subTab').hide()
+
+  //当前精灵组选择状态
+  this.parent.children[1].visible = true
+  this.parent.children[2].visible = true
+  this.parent.children[3].visible = true
+  // 调整序列
+  app.stage.setChildIndex(this.parent, app.stage.children.length - 1)
+  if (flag && this.name != 'resizeBtn') {
+    flag = false
+    startPos = JSON.parse(JSON.stringify(e.data.global))
+    objPos = this.parent.getGlobalPosition()
+    sprName = this.parent.name
+  }
+
+  //如果是新郎或者新娘 显示二级菜单
+  if (sprName == 'xinlangSuper') {
+    $('.subTab').eq(0).show()
+  }
+
+  if (sprName == 'xinniangSuper') {
+    $('.subTab').eq(3).show()
+  }
+}
+function drapdropTouchMove(e) {
+  if (this.name != 'resizeBtn' && this.parent.name == sprName) {
+    let temPos = e.data.global
+
+    this.parent.position.set(
+      objPos.x + (temPos.x - startPos.x),
+      objPos.y + (temPos.y - startPos.y)
+    )
+  }
+}
+function clearTouchend() {
+  flag = true
+  sprName = ''
+  resizeSprName = ''
+  app.stage.getChildByName('bg').off('touchmove')
+  app.stage.getChildByName('bg').interactive = false
+}
+
+// 缩放
+let resizeStartPosx = 0
+let resizeSprName = ''
+
+function resizeTouchStart(e) {
+  if (flag) {
+    resizeStartPosx = e.data.global.x
+    resizeSprName = this.parent.name
+    app.stage.getChildByName('bg').interactive = true
+    app.stage
+      .getChildByName('bg')
+      .on('touchmove', resizeTouchMove)
+      .on('touchend', clearTouchend)
+  }
+}
+function resizeTouchMove(e) {
+  if (this.name == 'bg') {
+    let tempPosX = e.data.global.x
+
+    let dur = tempPosX - resizeStartPosx
+    resizeStartPosx = tempPosX
+
+    let sprite = app.stage.getChildByName(resizeSprName)
+    if (dur > 0) {
+      let scale = sprite.scale.x + 0.01
+      // 放大
+      if (scale <= 1.5) {
+        sprite.scale.set(scale, scale)
+      }
+    }
+    if (dur < 0) {
+      // 缩小
+      if (sprite.scale.x >= 0.4) {
+        let scale = sprite.scale.x - 0.01
+
+        sprite.scale.set(scale, scale)
+      }
+    }
+  }
+}
+// 删除
+function del() {
+  app.stage.removeChild(this.parent)
+}
+// 生成海报
+function makePoster() {
+  // 清除选择状态
+  clearSelect()
+  setTimeout(() => {
+    let app2 = new PIXI.Application({
+      width: 750,
+      height: 1334,
+      transparent: true,
+    })
+    document.getElementById('stage').appendChild(app2.view)
+
+    app.render()
+
+    let poster = app.view.toDataURL('image/png')
+    let imgBase64 = new PIXI.Sprite.fromImage(poster)
+    imgBase64.position.set(0, 0)
+    imgBase64.width = 750
+    imgBase64.height = 1334
+    app2.stage.addChild(imgBase64)
+    setTimeout(() => {
+      let txt = new PIXI.Sprite.fromImage('../images/txt.png')
+      txt.position.set(0, 1089)
+      txt.width = 750
+      txt.height = 245
+      app2.stage.addChild(txt)
+
+      let rand = Math.floor(words.length * Math.random())
+      let word = words[rand]
+      let wordStyle = {
+        fontSize: '22px',
+        fill: '#5e7091',
+      }
+      let myword = new PIXI.Text(word, wordStyle)
+      myword.position.set(47, 139 + 1089)
+
+      app2.stage.addChild(myword)
+
+      setTimeout(() => {
+        let imgPoster = app2.renderer.plugins.extract.base64(app2.stage)
+        $('#myPoster').attr('src', imgPoster)
+      }, 200)
+    }, 200)
+  }, 200)
+}
+$('.saveBtn').bind('touchend', makePoster)
+
+// 音乐
+let music = document.getElementById('bgMusic')
+let musciState = true //是否播放中
+
+function musicPlay() {
+  musciState = true
+  sound.play()
+  $('.music').removeClass('musicPause').addClass('musicPlay')
+}
+
+function musicPause() {
+  musciState = false
+  sound.pause()
+  $('.music').removeClass('musicPlay').addClass('musicPause')
+}
+
+$('.music').bind('touchend', function () {
+  if (musciState) {
+    musicPause()
+  } else {
+    musicPlay()
+  }
+})
+
+var sound = new Howl({
+  src: ['./mp3/bg.mp3'],
+  autoplay: true,
+  loop: true,
+})
+
+sound.once('load', function () {
+  sound.play()
+  console.log('bgm loaded!')
+})
+
+sound.on('play', function () {
+  musciState = true
+  $('.music').removeClass('musicPause').addClass('musicPlay')
+})
+
+sound.on('pause', function () {
+  musciState = false
+  $('.music').removeClass('musicPlay').addClass('musicPause')
+})
+
+document.addEventListener(
+  'WeixinJSBridgeReady',
+  function () {
+    if (!musciState) {
+      sound.play()
+      sound.loop = true
+    }
+  },
+  false
+)
+
+// 分享和在玩一下
+
+$('.replayBtn').bind('touchend', function () {
+  window.location.href = window.location.href + '?n=' + Math.random()
+})
+$('.shareBtn').bind('touchend', function () {
+  $('.sharelayer').show()
+})
+$('.sharelayer').bind('touchend', function () {
+  $('.sharelayer').hide()
+})
